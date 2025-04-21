15b8aa739bf1db5a13868ce9686d9ffe0acfe7a5
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-04-21 18:10:14 +0200

    chore: initial commit adding src, include, ci/cd, cmakelist and gitignore

---
+++ include/raytracer.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** Raytracer
+** File description:
+** 04
+*/
+
+#ifndef RAYTRACER_HPP
+#define RAYTRACER_HPP
+
+namespace ray {};
+
+#endif //RAYTRACER_HPP
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** Raytracer
+** File description:
+** 04
+*/
+
+#include <cstdio>
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+};
