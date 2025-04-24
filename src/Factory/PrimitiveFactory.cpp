4a7e52725c2a6456d821365ed3dc995fcb9cd71f
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-04-24 23:44:43 +0200

    refactor: update factory type strings for consistency in PrimitiveFactory

--- src/Factory/PrimitiveFactory.cpp
+++ src/Factory/PrimitiveFactory.cpp
@@ -8,14 +8,18 @@
 #include "PrimitiveFactory.hpp"
 
 #include <unordered_map>
+#include "SphereFactory.hpp"
+#include "PlaneFactory.hpp"
+#include "CylinderFactory.hpp"
+#include "ConeFactory.hpp"
 
 std::shared_ptr<RayTracer::IPrimitiveFactory> RayTracer::IPrimitiveFactory::getFactory(const std::string& type)
 {
     static std::unordered_map<std::string, std::shared_ptr<IPrimitiveFactory>> factories = {
-        {"sphere", std::make_shared<SphereFactory>()},
-        {"plane", std::make_shared<PlaneFactory>()},
-        {"cylinder", std::make_shared<CylinderFactory>()},
-        {"cone", std::make_shared<ConeFactory>()},
+        {"spheres", std::make_shared<SphereFactory>()},
+        {"planes", std::make_shared<PlaneFactory>()},
+        {"cylinders", std::make_shared<CylinderFactory>()},
+        {"cones", std::make_shared<ConeFactory>()},
     };
 
     auto it = factories.find(type);
