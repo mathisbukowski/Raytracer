e853101f6503fd730df20456fc53ebeccb61e845
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-04-25 16:33:55 +0200

    feat: add AmbientLight class for ambient light representation

---
+++ src/Lights/AmbientLight.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2025
+** raytracer
+** File description:
+** 04
+*/
+
+#ifndef AMBIENTLIGHT_HPP
+#define AMBIENTLIGHT_HPP
+
+
+#include "ILight.hpp"
+
+namespace RayTracer {
+    class AmbientLight : public ILight {
+    private:
+
+    public:
+        AmbientLight()
+    };
+}
+
+
+#endif //AMBIENTLIGHT_HPP
