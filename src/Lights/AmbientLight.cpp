cdf71a4f4f355d47e7e03ccc04f72f27f341c74c
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-04-25 19:53:43 +0200

    feat: implement AmbientLight class with illumination calculation and properties

--- src/Lights/AmbientLight.cpp
+++ src/Lights/AmbientLight.cpp
@@ -6,3 +6,41 @@
 */
 
 #include "AmbientLight.hpp"
+
+namespace RayTracer {
+    Color AmbientLight::calculateIllumination(const Point3D& point, const Vector3D& normal, const Vector3D& viewDirection) const
+    {
+        Vector3D colorVector = this->getColor() * this->getIntensity();
+        return Color(colorVector.x, colorVector.y, colorVector.z);
+    }
+
+    bool AmbientLight::canCastShadow() const
+    {
+        return false;
+    }
+
+    Vector3D AmbientLight::getColor() const
+    {
+        return _color;
+    }
+
+    float AmbientLight::getIntensity() const
+    {
+        return _intensity;
+    }
+
+    Vector3D AmbientLight::getLightDirection(const Point3D& point) const
+    {
+        return Vector3D(0, 0, 0);
+    }
+
+    Vector3D AmbientLight::getPosition() const
+    {
+        return Vector3D(0, 0, 0);
+    }
+
+    bool AmbientLight::isAmbient() const
+    {
+        return true;
+    }
+}
--- src/Lights/AmbientLight.hpp
+++ src/Lights/AmbientLight.hpp
@@ -13,10 +13,22 @@
 
 namespace RayTracer {
     class AmbientLight : public ILight {
-    private:
-
+        float _intensity;
+        Vector3D _color;
     public:
-        AmbientLight()
+        explicit AmbientLight(float intensity = 0.4f, const Vector3D& color = Vector3D(1.0f, 1.0f, 1.0f))
+            : _intensity(intensity), _color(color) {}
+
+        Vector3D getPosition() const override;
+        float getIntensity() const override;
+        Vector3D getColor() const override;
+        bool isAmbient() const override;
+
+        Color calculateIllumination(const Point3D& point, const Vector3D& normal, const Vector3D& viewDirection) const override;
+
+        Vector3D getLightDirection(const Point3D& point) const override;
+
+        bool canCastShadow() const override;
     };
 }
 
